95e580f840a05363d8d4fc75bf64cbb02bb9bcd7
Seoyoung Lee <wow9176@naver.com>
2022-08-21 05:18:38 +0900

    feat: 문제 목록 데이터 받아오기

--- junction-dabda/junction-dabda/Controller/MainViewController.swift
+++ junction-dabda/junction-dabda/Controller/MainViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 private let cellIdentifier = "MainCell"
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var quizList: [QuizModel] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         configureNavigationBar()
+        loadQuizData()
     }
 
     @IBAction func pressedCreateButton() {
@@ -36,22 +44,41 @@ class MainViewController: UIViewController {
         
         navigationItem.backButtonTitle = ""
     }
+    
+    func loadQuizData() {
+        QuizService.shared.getAllQuizs { data in
+            guard let data = data.value else {
+                print("DEBUG - Fail to load data")
+                return
+            }
+            guard let quizData = try? JSONDecoder().decode([QuizModel].self, from: data) else {
+                print("DEBUG - Fail to decode")
+                return
+            }
+            self.quizList = quizData
+        }
+    }
 
 }
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return quizList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "MainCell", for: indexPath) as? MainTableViewCell else { return UITableViewCell() }
+        let quiz = quizList[indexPath.row]
         cell.profileImageView.image = ProfileImageList.imageList[Int.random(in: 0...8)]
-        cell.titleLabel.text = "문제 제목"
-        cell.questionId.text = "#1"
-        cell.writerLabel.text = "닉네임"
-        cell.scoreLabel.text = "5점"
+        cell.titleLabel.text = quiz.title
+        cell.questionId.text = "#\(quiz.id)"
+        cell.writerLabel.text = String(quiz.user) + "번님의 문제"
+        cell.scoreLabel.text = "\(quiz.quizScore)점"
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
     }
 }
 
